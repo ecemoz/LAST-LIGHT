c502fb908f571ff7c1c1147d78ad507dbb9d20cf
ecemoz <ecemnurozen03@gmail.com>
2026-06-23 21:26:34 +0300

    database.py dosyası ile fastapi ve postgresql arasında bir bağlantı yolu oluşturdum sql alchemy postgresql ile konuşmayı sağlayan bir araç olduğundan kullanıldı

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Last Light API"
     VERSION: str = "0.1.0"
+    DATABASE_URL: str
 
     class Config:
         env_file = ".env"
