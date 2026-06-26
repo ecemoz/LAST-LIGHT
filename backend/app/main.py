9d3ba997e61e14eb6f8df89ee24ef86b8da6a402
ecemoz <ecemnurozen03@gmail.com>
2026-06-26 18:56:35 +0300

    Kullanıcı modeli eklendi ve veritabanı tabloları oluşturuldu

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.core.database import engine, Base
+from app.models.user import User 
+
+
+Base.metadata.create_all(bind=engine)  # Tabloları oluşturmak için
 
 app = FastAPI(
    title = settings.PROJECT_NAME,
